6ce6deff739bb72925a90a217dfde7c1d0349003
Tyrone Marhguy <tmarhguy@seas.upenn.edu>
2024-10-26 12:56:47 -0400

    Revising Ocaml from basics to advanced

--- revising_ocaml.ml
+++ revising_ocaml.ml
@@ -1,3 +1,16 @@
 let x = 7 and y = 4;
 
+<<<<<<< HEAD
 ;;print_int (x+7)
+=======
+;;print_endline("The sum of x and y is " ^ string_of_int (x+y))
+
+let prod = 
+
+  let a = 9 in
+  let b = 10 in
+
+  a * b
+
+  ;;print_endline("Product of a and b is " ^ string_of_int(prod))
+>>>>>>> dcb76f7 (Revising Ocaml from basics to advanced)
