0db1d280665a22bb6a12e782ac6068d6cca5c6ee
Tyrone Marhguy <tmarhguy@seas.upenn.edu>
2024-10-26 13:20:59 -0400

    fibo in ocaml implemented

--- revising_ocaml.ml
+++ revising_ocaml.ml
@@ -1,9 +1,8 @@
+let printer (message: string)(value: int):unit =
+  print_endline(message ^": "^string_of_int(value))
 let x = 7 and y = 4;
 
-<<<<<<< HEAD
-;;print_int (x+7)
-=======
-;;print_endline("The sum of x and y is " ^ string_of_int (x+y))
+;;printer "Sum of 7 and 4 is "(x + y)
 
 let prod = 
 
@@ -12,5 +11,26 @@ let prod =
 
   a * b
 
-  ;;print_endline("Product of a and b is " ^ string_of_int(prod))
->>>>>>> dcb76f7 (Revising Ocaml from basics to advanced)
+;;printer "Product of 9 and 10 is "prod
+
+(*implementing fibonacci *)
+
+let rec fibo(x: int): int = 
+  begin match x with
+  |1 -> 0
+  |2 -> 1
+  |3 -> 1
+  |4 -> 2
+  |_ -> fibo(x-1) + fibo(x-2)
+end
+
+;;printer "1st fibonacci number is " (fibo 1)
+;;printer "2nd fibonacci number is " (fibo 2)
+;;printer "3rd fibonacci number is " (fibo 3)
+;;printer "4th fibonacci number is " (fibo 4)
+;;printer "5th fibonacci number is " (fibo 5)
+;;printer "6th fibonacci number is " (fibo 6)
+;;printer "7th fibonacci number is " (fibo 7)
+
+
+
